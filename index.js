ff6ab6b5c200e0a4a9118173abde19ba41b92702
juhayerayaz <aiazj97@gmail.com>
2022-05-24 04:08:25 +0600

    items shown and purchase implemented

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const res = require('express/lib/response');
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -44,16 +43,67 @@ async function run() {
 
         await client.connect();
         const userCollection = client.db('drilled_tools').collection('users');
+        const itemsCollection = client.db('drilled_tools').collection('items');
 
 
+        // getting all items
 
-        //  getting user information
+
+        app.get('/items', async (req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        app.get('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemsCollection.findOne(query);
+            res.send(item);
+        });
+
+        // POST
+        app.post('/items', async (req, res) => {
+            const newItem = req.body;
+            const result = await itemsCollection.insertOne(newItem);
+            res.send(result);
+        });
+
+        // update user
+        app.put('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedUser = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedUser.quantity
+                }
+            };
+            const result = await itemsCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+
+        })
+
+
+        // DELETE
+        app.delete('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemsCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+        //  getting user information via login and signup
         app.get('/user', verifyJWT, async (req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users);
         })
 
         app.put('/user/:email', async (req, res) => {
+            const name = req.body;
             const email = req.params.email;
             const user = req.body;
             const filter = { email: email };
@@ -61,7 +111,7 @@ async function run() {
             const updatedDoc = {
                 $set: user,
             };
-            const result = await userCollection.updateOne(filter, updatedDoc, options);
+            const result = await userCollection.updateOne(filter, updatedDoc, options, name);
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
             res.send({ result, token });
         })
